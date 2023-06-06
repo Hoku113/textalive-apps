fc8964a58bfea7fa6cb5894541744007481a2fd0
Hoku <hokuto.u0113@gmail.com>
2023-06-06 21:18:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Player } from 'textalive-app-api';
+
+function App() {
+  return (
+    <div className="App">
+      <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+      <script src="https://unpkg.com/textalive-app-api/dist/index.js"></script>
+      <script>
+        const { Player } = TextAliveApp;
+      </script>
+    </div>
+  );
+}
+
+export default App;
